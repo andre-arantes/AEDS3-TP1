5c9fbfda9442a8c9c579864042a6f4255a63d221
André <andre2004bh@gmail.com>
2023-02-27 11:49:46 -0300

    tp 01 incompleto

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 
 import java.io.*;
-// import java.io.FileNotFoundException;    
-import java.io.RandomAccessFile;
 import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        RandomAccessFile raf = new RandomAccessFile("db/movies.db", "rw");
+        RandomAccessFile raf = new RandomAccessFile("./db/movie.db", "rw");
         Scanner sc = new Scanner(System.in);
         if (raf.length() == 0)
-            raf.write(0); 
+            raf.write(0);
         raf.seek(0); // Volta para o inicio do arquivo
         boolean loop = true;
         int opcao = 0;
@@ -40,7 +38,7 @@ class Main {
                     System.out.println("Teste");
             }
         }
-
+        raf.close();
         sc.close();
     }
 }
